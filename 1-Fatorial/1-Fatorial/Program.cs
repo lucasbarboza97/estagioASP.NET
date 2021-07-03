e4efeb5056142216c821b6fce534f55a5f5f8414
lucasbarboza97 <73370097+lucasbarboza97@users.noreply.github.com>
2021-07-03 11:37:17 -0300

    Add files via upload
    Criado projeto para a questão 1.

---
+++ 1-Fatorial/1-Fatorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int calcularFatorial(int x)
+            {
+                int i,  fatorial;
+                fatorial = x;
+                for (i = x - 1; i >= 1; i--)
+                {
+                    fatorial = fatorial * i;
+                }
+                return fatorial;
+
+            }
+            int valor = 5;
+            int resultado = calcularFatorial(valor);
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+    }
+}
